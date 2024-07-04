e886eead4f801fa39356f547c3a580f5e3b80dd6
1DS22IS120-RohithBN <rohithbn27@gmail.com>
2024-07-04 13:34:41 +0530

    Login page and home page 2.0

--- app/routes.py
+++ app/routes.py
@@ -28,7 +28,7 @@ def register():
 @bp.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('routes.home'))
+        return redirect(url_for('routes.dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
